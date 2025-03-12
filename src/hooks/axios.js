2f51a8f8bcbda0e29c20770c81239fac52a98610
chathurajanithya <it21263880@my.sliit.lk>
2025-03-12 13:48:44 +0530

    if the token is invalid redirect to the login

--- src/hooks/axios.js
+++ src/hooks/axios.js
@@ -13,4 +13,22 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
+// Add response interceptor to handle token expiration
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response; // If the request is successful, just return the response
+  },
+  async (error) => {
+    // If the error is due to an expired token (403 Unauthorized)
+    if (error.response && error.response.status === 403) {
+      // Clear the expired token from localStorage
+      localStorage.removeItem("token");
+
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
